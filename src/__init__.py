4474bc82028fb4c7484aa93288e51f5a89be4aa3
Guga Coder <guga.coder@gmail.com>
2025-08-15 16:17:29 +0000

    Complete reset of main branch

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-NIC Chat System - Main Package
-
-Corporate AI platform built on Open WebUI that interfaces with GitLab
-repositories for knowledge base queries and document generation.
-"""
-
-__version__ = "1.0.0"
-__author__ = "NIC Chat Team"
